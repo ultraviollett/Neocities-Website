49fbc4e87b9f7debce60b4726cc3f7ac2fe0e9da
Lonny <86886983+ultraviollett@users.noreply.github.com>
2023-11-29 17:22:29 +0000

    etc

--- ao3generatorwebpage/app.js
+++ ao3generatorwebpage/app.js
@@ -1,29 +1,69 @@
-function favTutorial() {  
+var mcchoice;
+var fullthing;
+var mctext;
+
+function mcchoice() {  
     var mylist = document.getElementById("myList");  
     document.getElementById("favourite").value = mylist.options[mylist.selectedIndex].text;  
     
     var mcchoice = mylist.options[mylist.selectedIndex].text;
     switch(mcchoice){
         case "Minecraft Button":
-        document.getElementById("favourite").value = "button";  
+        document.getElementById("favourite").innerHTML = "button";  
+        document.getElementById("classchange").className = "buttonbg"
+        document.getElementById("paste").className = "buttontext";
         break;
+
         case "Minecraft End Poem":
-        document.getElementById("favourite").value = "poem";  
+        document.getElementById("favourite").innerHTML = "poem";  
+        document.getElementById("classchange").className = "poembg";
+        document.getElementById("paste").className = "poemtextwhite";
         break;
+
         case "Minecraft Book":
-        document.getElementById("favourite").value = "book";  
+        document.getElementById("favourite").innerHTML = "book";  
+        document.getElementById("classchange").className = "mcbook";
+        document.getElementById("paste").className = "";
         break;
-    
-    
     }
+    return mcchoice;
 
 } 
 
-function myFunction() {
-    var x = document.getElementById("myText").value;
-    document.getElementById("demo").innerHTML = x;
+function mcform() {
+    var mctext = document.getElementById("mctext").value;
+    document.getElementById("paste").innerHTML = mctext;
+    return mctext
+
+
   }
 
+function generateHTML(){
+
+    var fullthing = "";
+
+    switch(mcchoice()){
+        case "Minecraft Button":
+        fullthing = '<div class="buttonbg"><p class="buttontext">';
+        break;
+
+        case "Minecraft End Poem":
+        fullthing = '<div class="poembg"><p class="poemtextwhite">';
+        break;
+
+        case "Minecraft Book":
+        fullthing = '<div class="mcbook"><p>';
+        break;        
+
+        default:
+        fullthing = '<div><p>';
+        break;
+    }
+
+    var fullthing = fullthing.concat(mcform()+"</p></div>");
+
+    document.getElementById("fullthing").value = fullthing;
+}
 
 function copyText() {
     // Get the text field
