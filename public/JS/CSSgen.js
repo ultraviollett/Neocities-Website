841a3b179632954f400f0a87268777b0c86f356c
Lonny <86886983+ultraviollett@users.noreply.github.com>
2023-11-30 16:10:50 +0000

    [GEN] adding CSS generator

---
+++ public/JS/CSSgen.js
@@ -0,0 +1,10 @@
+document.getElementById("mcbuttonCSS").style.display = "none";
+
+function mcbutton(){
+let x = document.getElementById("mcbuttonCSS");
+if (x.style.display === "none") {
+  x.style.display = "block";
+} else {
+  x.style.display = "none";
+}
+} 
