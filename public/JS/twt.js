0012be05301fed4b52ca7f9a283b2265b9704f39
Lonny <86886983+ultraviollett@users.noreply.github.com>
2023-12-03 11:46:58 +0000

    [ADD] twitter update to useable and contact form

--- public/JS/twt.js
+++ public/JS/twt.js
@@ -9,7 +9,8 @@ document.getElementById("qpolloptions").style.display = "none";
 
 var prev = " "; //holds entire HTML before next is added
 var childcount = 0; //stores the number of tweets, so you know when to add twtstart
-var tweetop = ""; //records the name of the original poster for replies
+var tweetop = ""; //automatically reply to op
+
 
 function roundnum(x){
 //rounds numbers so it obeys the twitter format, ie 123, 54K, 45M, etc
@@ -24,15 +25,11 @@ function roundnum(x){
 }
 
 function twttype(){
-    if (!tweetop && $("#twttype").val() == 'Reply to tweet'){
+    if ($("#twttype").val() == 'Reply to tweet'){
         document.getElementById("replynotweet").style.display = "";
+        document.getElementById("time").style.display = "none";
     }else{
-        document.getElementById("replynotweet").style.display = "none"
-    }
-
-    if($("#twttype").val() == 'Reply to tweet'){
-        document.getElementById("time").style.display = "none"
-    }else{
+        document.getElementById("replynotweet").style.display = "none";
         document.getElementById("time").style.display = "";
     }
 }
@@ -57,10 +54,7 @@ function username(){
 
 function twturl(){
     let x = document.getElementById("twturl").value;
-    if($("#twttype").val() == 'Tweet'){
-        tweetop = x;
-    }
-
+    if ($("#twttype").val() == 'Tweet'){tweetop = x;}
     return x;
 
 
@@ -103,23 +97,100 @@ function like(){
 
 //images
 
-function images(x){
-    if($("#image1").val() && $("#image2").val()){ 
+function images(x,i1="",i2=""){
+    if(i1 && i2){ 
         //if there are two images
         x = x + '<div class="twt-image imagesmulti"> <div style = "background-image: url(';
-        x = x + " ' "+$("#image1").val()+"'";
+        x = x + " ' "+i1+"'";
         x = x + ')"></div><div style = "background-image: url('
-        x = x + " ' "+$("#image2").val()+"'";
+        x = x + " ' "+i2+"'";
         x = x + ')"></div> </div> '; 
 
-    }else if($("#image1").val()){
+    }else if(i1){
         //if only one image is there
         x = x + '<div class="twt-image"> <div style = "background-image: url(';
-        x = x + " ' "+$("#image1").val()+"'";
+        x = x + " ' "+i1+"'";
         x = x + ')"></div></div>';
     }
     return x
 }
+
+//polls
+
+function poll(x,p1="",p2="",p3="",p4="",votes=""){
+    let ispoll = false;
+    if(p1){
+        x = x + ' <div class="twtpoll"><span class="twtpolltext">'+p1+'</span></div>'
+        ispoll = true;
+    }
+    if(p2){
+        x = x + ' <div class="twtpoll"><span class="twtpolltext">'+p2+'</span></div>'
+        ispoll = true;
+    }
+    if(p3){
+        x = x + ' <div class="twtpoll"><span class="twtpolltext">'+p3+'</span></div>'
+        ispoll = true;
+    }
+    if(p4){
+        x = x + ' <div class="twtpoll"><span class="twtpolltext">'+p4+'</span></div>'
+        ispoll = true;
+    }
+    if (ispoll && votes){
+        let votenum = roundnum(votes);
+        x = x + '<span class="pollvotes">'+votenum+' votes</span>';
+    }
+    return x;
+}
+
+//quotetweet
+
+
+function quotetweet(x){
+
+    x = x + '<div class="twt-quotebox"> <div class="twt-header"> ';
+    if($("#qicon").val()){
+        x = x + '<div class="twt-icon-container"> <img class="twt-iconquote" src="'+$("#qicon").val()+'"> </div> ';
+    }
+    x = x + ' <div class="twt-id"> <span class="twt-name"> '+$("#qusername").val()+'</span> <br/>';
+    
+    if(twturl()){
+    x = x + '<span class="twt-handle">@'+twturl();
+    }
+    if($("#qdate").val()){
+        x = x + " · "+ $("#qdate").val();
+    }
+    x = x +'</span> </div> </div> <div class="twt-contentquote"> '+$("#qtweet").val()+' </div>';
+
+    x = images(x,$("#qimage1").val(),$("#qimage2").val());
+    x = poll(x,$("#qpoll1").val(),$("#qpoll2").val(),$("#qpoll3").val(),$("#qpoll4").val(),$("#qvotes").val());
+    
+    x = x + '</div>';
+
+    return x;
+/* 
+    <div class="twt-quotebox"> <div class="twt-header"> 
+    <div class="twt-icon-container"> <img class="twt-iconquote" src="'+icon()+'"> </div>
+
+<div class="twt-id">
+<span class="twt-name">barry</span> 
+<span class="twt-handle">@donkey · Sept 5</span>
+            </div>
+        </div>
+        
+        <div class="twt-contentquote">i'm donkey!</div>
+    
+    </div>
+    
+    <div class="twt-timestamp">8:14 AM · Oct 2, 2020</div>
+    
+    <hr class="twt-sep"><div class="twt-stat1"><strong>23.3K
+    </strong> Retweets &nbsp;&nbsp; <strong>924</strong> 
+    Quote Tweets &nbsp;&nbsp; <strong>72.1K</strong> Likes
+    </div>    
+    
+*/
+}
+
 
 //function to compile the entire tweet, including previous
 
@@ -147,13 +218,21 @@ function newtweet(previous){
     x = x + '<div class="twt-icon-container"> <img class="twt-icon" src="'+icon()+'"> </div>';
     }
     
-    x = x + ' <div class="twt-id"> <span class="twt-name"> '+username()+'</span> <br/> <span class="twt-handle">@'+twturl()+'</span> </div> </div> <div class="twt-content"> '+tweet()+' </div>';
+    x = x + ' <div class="twt-id"> <span class="twt-name"> '+username();
 
-    x = images(x);
+    if(twturl()){
+        x = x + '<span class="twt-handle">@'+twturl();
+    }
 
-    //if ($('input[id="quotetweetopt"]:checked').val()){
-      //  x = x + "WQUOTE";
-    //}
+    x = x +'</span> <br/></div> </div> <div class="twt-content"> '+tweet()+' </div>';
+
+    x = images(x,$("#image1").val(),$("#image2").val());
+
+    x = poll(x,$("#poll1").val(),$("#poll2").val(),$("#poll3").val(),$("#poll4").val(),$("#votes").val());
+
+    if ($('input[id="quotetweetopt"]:checked').val()){
+        x = quotetweet(x);
+    }
 
     if (time() && date()){
         x = x + '<div class="twt-timestamp"> '+time()+' · '+date()+'</div>';
@@ -163,7 +242,7 @@ function newtweet(previous){
         x = x + '<div class="twt-timestamp"> '+date()+'</div>';
     }
  
-    x = x + '<hr class="twt-sep"> <div class="twt-stat1"> <img class="twt-socialimg" src="https://i.imgur.com/dJg9v1v.png"> <strong>'+reply()+'</strong></p><p> <img class="twt-socialimg" src="https://i.imgur.com/dJg9v1v.png"> <strong>'+retweet()+' </strong> </p><p><strong> <img class="twt-socialimg" src="https://i.imgur.com/eM56CN2.png">   '+like()+'</strong> </p><p><span class="twtsharebutton">↥</span> </p> </div> </div>';
+    x = x + '<hr class="twt-sep"> <div class="twt-stat1"> <img class="twt-socialimg" src="https://i.imgur.com/dJg9v1v.png"> <strong>'+reply()+'</strong></p><p> <img class="twt-socialimg" src="https://i.imgur.com/dJg9v1v.png"> <strong>'+retweet()+' </strong> </p><p><strong> <img class="twt-socialimg" src="https://i.imgur.com/eM56CN2.png">   '+like()+'</strong> </p></div> </div>';
  
    
         return (x);
@@ -187,69 +266,39 @@ function newreply(previous){
 
     x = x + '"> <div class="twt-replybox">';
 
-    if (icon() != ""){
+    if (icon()){
         x = x + '<div class="twt-icon-replycontainer"> <img class="twt-icon" src="'+icon()+'"> </div>';
     }
-    x = x + '<div class="twt-replycontainer"> <span class="twt-name">'+username()+'</span> <span class="twt-handle">@'+twturl();
+    x = x + '<div class="twt-replycontainer"> <span class="twt-name">'+username()+'</span>';
+    
+    if(twturl()){
+        x = x + '<span class="twt-handle">@'+twturl();
+    }
 
     if (date()){
         x = x + ' · '+date() + '</span>';
     }
 
-    if(tweetop){
-        x = x + '<span class="twt-handle"> Replying to</span> <span class="twt-hl">@'+tweetop+'</span>';
-    }else if(opurl()){
+    if(opurl()){
         x = x + '<span class="twt-handle"> Replying to</span> <span class="twt-hl">@'+opurl()+'</span>';
+    }else if (tweetop){
+        x = x + '<span class="twt-handle"> Replying to</span> <span class="twt-hl">@'+tweetop+'</span>';
     }
     
     x = x + '<div class="twt-replycontent">'+tweet()+'</div>';
 
-    x = images(x);
+    x = images(x,$("#image1").val(),$("#image2").val());
+    x = poll(x,$("#poll1").val(),$("#poll2").val(),$("#poll3").val(),$("#poll4").val(),$("#votes").val());
 
-    x = x + '<div class="twt-social"><img class="twt-socialimg" src="https://i.imgur.com/dJg9v1v.png">'+reply()+'</div>';
-    x = x + '<div class="twt-social"><img class="twt-socialimg" src="https://i.imgur.com/UeOnwXk.png">'+retweet()+'</div>';
-    x = x + '<div class="twt-social"><img class="twt-socialimg" src="https://i.imgur.com/eM56CN2.png">'+like()+'</div> </div> </div> </div>';
-
+    //x = x + '<div class="twt-social"><img class="twt-socialimg" src="https://i.imgur.com/dJg9v1v.png">'+reply()+'</div>';
+    //x = x + '<div class="twt-social"><img class="twt-socialimg" src="https://i.imgur.com/UeOnwXk.png">'+retweet()+'</div>';
+    //x = x + '<div class="twt-social"><img class="twt-socialimg" src="https://i.imgur.com/eM56CN2.png">'+like()+'</div> </div> </div> </div>';
+    x = x + '<div class="twt-stat1"> <img class="twt-socialimg" src="https://i.imgur.com/dJg9v1v.png"> <strong>'+reply()+'</strong></p><p> <img class="twt-socialimg" src="https://i.imgur.com/dJg9v1v.png"> <strong>'+retweet()+' </strong> </p><p><strong> <img class="twt-socialimg" src="https://i.imgur.com/eM56CN2.png">   '+like()+'</strong> </p></div> </div>';
     return(x);
 
 }
 
 
-function quotetweet(){
-
-    x = x + '<div class="twt-quotebox"> <div class="twt-header"> ';
-    if(qicon()){
-        x = x + '<div class="twt-icon-container"> <img class="twt-iconquote" src="'+qicon()+'"> </div> ';
-    }
-    x = x + ' <div class="twt-id"> <span class="twt-name"> '+qusername()+'</span> <br/> <span class="twt-handle">@'+twturl();
-
-    if(qdate()){
-        x = x + " · "+ qdate();
-    }
-    x = x +'</span> </div> </div> <div class="twt-contentquote"> '+qtweet()+' </div></div>';
-/* 
-    <div class="twt-quotebox"> <div class="twt-header"> 
-    <div class="twt-icon-container"> <img class="twt-iconquote" src="'+icon()+'"> </div>
-
-<div class="twt-id">
-<span class="twt-name">barry</span> 
-<span class="twt-handle">@donkey · Sept 5</span>
-            </div>
-        </div>
-        
-        <div class="twt-contentquote">i'm donkey!</div>
-    
-    </div>
-    
-    <div class="twt-timestamp">8:14 AM · Oct 2, 2020</div>
-    
-    <hr class="twt-sep"><div class="twt-stat1"><strong>23.3K
-    </strong> Retweets &nbsp;&nbsp; <strong>924</strong> 
-    Quote Tweets &nbsp;&nbsp; <strong>72.1K</strong> Likes
-    </div>    
-    
-*/
-}
 
 //functions using jquery that generate the HTML to be used in the page and also put in the textbox
 
@@ -263,6 +312,35 @@ $(function() {
         }
 
         $('#twtdiv').html(x);
+
+        //now erase all that is needed
+        $('#tweet').val('');
+        $('#reply').val('');
+        $('#retweet').val('');
+        $('#like').val('');
+        $('#image1').val('');
+        $('#image2').val('');
+        $('#poll1').val('');
+        $('#poll2').val('');
+        $('#poll3').val('');
+        $('#poll4').val('');
+        $('#votes').val('');
+
+        //now for qtweet erasing
+        $('#qicon').val('');
+        $('#qusername').val('');
+        $('#qtwturl').val('');
+        $('#qtweet').val('');
+        $('#qtweet').val('');
+        $('#qimage1').val('');
+        $('#qimage2').val('');
+        $('#qdate').val('');
+        $('#qpoll1').val('');
+        $('#qpoll2').val('');
+        $('#qpoll3').val('');
+        $('#qpoll4').val('');
+        $('#qvotes').val('');
+
         prev = x;
     });
     
