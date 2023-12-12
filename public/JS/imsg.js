a5b24186ee32387ad99c0f9401ea77f5da950a69
Lonny <86886983+ultraviollett@users.noreply.github.com>
2023-12-12 03:53:31 +0000

    Adding iMessage

---
+++ public/JS/imsg.js
@@ -0,0 +1,16 @@
+
+document.getElementById("messengernamediv").style.display = "none";
+
+
+var currentname = "";
+
+
+/*
+$(function() {
+
+    $("#other").on("click", function() {
+        $("#messengername").toggle();//reveals polls
+    });
+});
+
+*/
