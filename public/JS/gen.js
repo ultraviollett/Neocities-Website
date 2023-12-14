6cb849c035a8b15c9905945b4f73124f86086004
Lonny <86886983+ultraviollett@users.noreply.github.com>
2023-12-14 20:51:32 +0000

    Process of updating generators to include both CSS and HTML

--- public/JS/gen.js
+++ public/JS/gen.js
@@ -2,15 +2,31 @@
 document.getElementsByTagName('input').checked = "false";
 
 //add back function that increases textareas vert to fit text
+textarea = document.querySelector(".autoresizing");
+textarea.addEventListener('input', autoResize, false);
+
+function autoResize() {
+    this.style.height = 'auto';
+    this.style.height = this.scrollHeight + 'px';
+}
 
 //https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Interact_with_the_clipboard 
 //read above over
+
+
 $(function() {
-    $('#copybutton').on('click', function() {
-      tocopy = $("#HTMLoutput").val()
+    $('#copyHTMLbutton').on('click', function() {
+      let tocopy = $("#HTMLoutput").val();
       navigator.clipboard.writeText(tocopy);
-      $("#copybutton").text("Copied!");
+      $("#copyHTMLbutton").text("Copied!");
+
   });
   
+  $('#copyCSSbutton').on('click', function() {
+    let tocopy = $("#CSStocopy").val();
+    navigator.clipboard.writeText(tocopy);
+    $("#copyCSSbutton").text("Copied!");
+});
+
 });
 
